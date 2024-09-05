a08851e9496783e590f4bb2f2fe3046fd8390824
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-09-05 15:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	var character='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var result='';
+	for(let i=0; i<l; i++){
+		result+=character.charAt(Math.floor(Math.random()*character.length));
+	}
+	return result;
 }
 
 // Do not change the code below.
